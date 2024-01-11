236b49833f3b995de34e5162cbd247346bdecb08
Tjoet Muty Ahmad <tjmutyahmd27@gmail.com>
2024-01-11 09:36:06 +0800

    comment of scope_param

--- project-songfy/src/helpers/config.js
+++ project-songfy/src/helpers/config.js
@@ -2,4 +2,4 @@ export const CLIENT_ID = 'bb814f9f64f14b91bcbc7ab3528c60fb'
 export const AUTHORIZE_ENDPOINT = 'https://accounts.spotify.com/authorize'
 export const REDIRECT_URI = 'http://localhost:4002/'
 export const SCOPE = ['user-top-read', 'user-read-recently-played']
-export const SCOPE_PARAM = SCOPE.join("%20") //spasi di link
+export const SCOPE_PARAM = SCOPE.join("%20") //spasi di link output: 'user-top-read%20user-read-recently-played'
