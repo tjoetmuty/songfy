89990f19ed7efc333186ef1bf5cce83dfea6e92e
Tjoet Muty Ahmad <tjmutyahmd27@gmail.com>
2024-01-18 13:45:09 +0800

    update navbar

--- project-songfy/src/components/Navbar/index.jsx
+++ project-songfy/src/components/Navbar/index.jsx
@@ -1,10 +1,30 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 const Navbar = () => {
   const [data, setData] = useState({})
-  const getMe 
+  const getMe = async () => {
+    try{
+      // eslint-disable-next-line no-undef
+      const res  = await axios.get('https://api.spotify.com/v1/me' , {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('access_token')}`
+        }
+      })
+      setData(res.data)
+      console.log(res)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    getMe()
+  }, [])
   return (
-    <div>Navbar</div>
+    <div>
+      <h1>Hi, {data?.display_name}</h1>
+      <img src={data?.images[1]?.url} alt="profile" className="rounded-full" />
+    </div>
   )
 }
 
