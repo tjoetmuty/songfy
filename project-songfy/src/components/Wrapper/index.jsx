5a45e47e63778fe027c7a24c8043e2c3fa91f6d1
Tjoet Muty Ahmad <tjmutyahmd27@gmail.com>
2024-01-18 14:09:46 +0800

    add new components

--- project-songfy/src/components/Wrapper/index.jsx
+++ project-songfy/src/components/Wrapper/index.jsx
@@ -1,19 +1,19 @@
-import { Outlet } from 'react-router-dom'
-import Navbar from '../Navbar'
-import Sidebar from '../Sidebar'
+import { Outlet } from "react-router-dom";
+import Navbar from "../Navbar";
+import Sidebar from "../Sidebar";
 
 const Wrapper = () => {
   return (
     <div>
-      <div className='flex'>
-      <Sidebar/>
-      <div className='w-full my-2 mr-2'>
-      <Navbar/>
-      <Outlet/>
-      </div>
+      <div className="flex">
+        <Sidebar />
+        <div className="w-full my-2 mr-2">
+          <Navbar/>
+          <Outlet />
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Wrapper
+export default Wrapper;
