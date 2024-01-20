f488c8e75770b624368fd71952578543643b97ba
Tjoet Muty Ahmad <tjmutyahmd27@gmail.com>
2024-01-20 11:45:51 +0800

    update design navbar

--- project-songfy/src/components/Navbar/index.jsx
+++ project-songfy/src/components/Navbar/index.jsx
@@ -1,32 +1,39 @@
-import { useEffect, useState } from "react"
-import axios from "axios"
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Navbar = () => {
-  const [data, setData] = useState({})
+  const [data, setData] = useState({});
   const getMe = async () => {
-    try{
+    try {
       // eslint-disable-next-line no-undef
-      const res  = await axios.get('https://api.spotify.com/v1/me' , {
+      const res = await axios.get("https://api.spotify.com/v1/me", {
         headers: {
-          Authorization: `Bearer ${localStorage.getItem('access_token')}`
-        }
-      })
-      setData(res.data)
-      console.log(res)
+          Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+        },
+      });
+      setData(res.data);
+      console.log(res);
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
-  }
+  };
 
   useEffect(() => {
-    getMe()
-  }, [])
+    getMe();
+  }, []);
   return (
     <div className="w-full h-20 bg-gray-400 rounded-xl flex justify-between items-center p-3">
-      <h1 className="font-bold">Hi, {data?.display_name}</h1>
-      <img src={data?.images?.url} alt="profile" className="rounded-full w-[50px] h-[50px]" />
+      <div className="flex gap-1">
+        <h1 className="font-bold">Welcome,</h1>
+        <p className="italic "> {data?.display_name}</p>
+      </div>
+      <img
+        src={data?.images[0]?.url}
+        alt="profile"
+        className="rounded-full w-[50px] h-[50px]"
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
