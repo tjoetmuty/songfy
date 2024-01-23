d12a14771777d939ecc65988b6eb20f0b91b3aea
Tjoet Muty Ahmad <tjmutyahmd27@gmail.com>
2024-01-23 20:57:54 +0800

    commit

--- project-songfy/src/App.jsx
+++ project-songfy/src/App.jsx
@@ -1,18 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import TopArtist from "./pages/TopArtist";
 import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
 import Wrapper from "./components/Wrapper";
+import DashboardFeatures from "./features/Dashboard";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />}></Route>
       <Route element={<Wrapper/>}>
-        <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route path="/dashboard" element={<DashboardFeatures />}></Route>
         <Route path="/top-artist" element={<TopArtist />}></Route>
-        <Route path="/top-music" element={<TopArtist />}></Route>
-        <Route path="/recently-played" element={<TopArtist />}></Route>
+        {/* <Route path="/top-music" element={<topMusic />}></Route>
+        <Route path="/recently-played" element={<TopArtist />}></Route> */}
       </Route>
     </Routes>
   );
