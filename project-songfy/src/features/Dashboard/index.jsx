094b6488e671a9a0617a04716a4dbc23b7fe437c
Tjoet Muty Ahmad <tjmutyahmd27@gmail.com>
2024-01-24 14:11:06 +0800

    first commit

--- project-songfy/src/features/Dashboard/index.jsx
+++ project-songfy/src/features/Dashboard/index.jsx
@@ -1,11 +1,28 @@
+import { useEffect, useState } from "react";
+
 const DashboardFeatures = () => {
+  const [data, setData] = useState({})
+  const getMe = async () => {
+    try {
+      // eslint-disable-next-line no-undef
+      const res = await axios.get("https://api.spotify.com/v1/browse/categories", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+        },
+      });
+      setData(123, res.data);
+      console.log(res);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getMe();
+  }, []);
   return (
-    <>
-    <div>
-      <div>ini dashboard</div>
-    </div>
-    </>
-  );
+  <div>ini dashboard</div>
+  )
 };
 
 export default DashboardFeatures;
