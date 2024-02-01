94611b78f4a8fdad7f52cd114297bcbf6705ab25
Tjoet Muty Ahmad <tjmutyahmd27@gmail.com>
2024-02-01 19:54:11 +0800

    fix: fix navbar's design

--- project-songfy/src/components/Navbar/index.jsx
+++ project-songfy/src/components/Navbar/index.jsx
@@ -29,14 +29,16 @@ const Navbar = () => {
       <div className="bg-black py-2">
         <div className="bg-[#5B3838] rounded-md mr-2">
           <div className="flex px-4 items-center justify-between">
-            <h1 className="text-white font-bold text-[30px]">Hello</h1>
+            <div className="flex items-center gap-2">
+              <h1 className="text-white font-thin text-[30px]">Welcome</h1>
+              <h1 className="text-white font-semibold text-[30px]">{data?.display_name}</h1>
+            </div>
             <div className="flex items-center gap-2">
               <img
                 src={images}
                 alt=""
-                className="rounded-full w-[30px] h-[30px] border-4 border-[#282828]"
+                className="rounded-full w-[35px] h-[35px] border-4 border-[#282828]"
               />
-              <h1 className="text-white">{data?.display_name}</h1>
             </div>
           </div>
         </div>
