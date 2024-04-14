c78490b302015d41bd3f92c2c59cbd70f01c4544
Tjoet Muty Ahmad <tjmutyahmd27@gmail.com>
2024-04-14 20:51:43 +0800

    add: new file

--- project-songfy/src/helpers/config.js
+++ project-songfy/src/helpers/config.js
@@ -1,5 +1,9 @@
-export const CLIENT_ID = 'bb814f9f64f14b91bcbc7ab3528c60fb'
+
+// eslint-disable-next-line no-undef
+export const CLIENT_ID = process.env.CLIENT_ID
 export const AUTHORIZE_ENDPOINT = 'https://accounts.spotify.com/authorize'
-export const REDIRECT_URI = 'http://localhost:4002/'
-export const SCOPE = ['user-top-read', 'user-read-recently-played']
+// eslint-disable-next-line no-undef
+export const REDIRECT_URI = process.env.REDIRECT_URI
+// eslint-disable-next-line no-undef
+export const SCOPE = process.env.SCOPE
 export const SCOPE_PARAM = SCOPE.join("%20") //spasi di link | output: 'user-top-read%20user-read-recently-played'
